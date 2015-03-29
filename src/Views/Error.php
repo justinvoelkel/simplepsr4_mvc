36c088637c617cb0f127dc05985d2c1e1e7c134c
justin voelkel <justin@budgetdumpster.com>
2015-03-29 14:48:40 -0400

    Added error view template

---
+++ src/Views/Error.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<head lang="en">
+    <meta charset="UTF-8">
+    <title>Something Went Wrong | SimplePSR4</title>
+</head>
+<body>
+<div class="container">
+    <div class="row">
+        <div class="col-md-12">
+            <h1>Oops, Sorry!</h1>
+            <p>The Record you're looking for wasn't found!</p>
+        </div>
+    </div>
+</div>
+</body>
+</html>
